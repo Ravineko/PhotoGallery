44cee013773036f96beb5d8b89e9c3c079679aa1
Yaroslav Bychkov <ybychkov@leobit.com>
2024-11-02 23:51:05 +0200

    feat: updated constants

--- Configurations/Constants/ConfigurationConstants.cs
+++ Configurations/Constants/ConfigurationConstants.cs
@@ -4,4 +4,5 @@ public static class ConfigurationConstants
     public const string JWTSettingsSection = "JWTSettings";
     public const string ImageSettingsSection = "ImageSettings";
     public const string CodeExpirySettingsSection = "CodeSettings";
+    public const string EmailSettingsSection = "EmailSettings";
 }
