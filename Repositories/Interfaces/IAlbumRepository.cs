4fd88c4ef741f70a84a7d9713241cb9cc5a87fd1
Yaroslav Bychkov <ybychkov@leobit.com>
2024-11-03 00:20:35 +0200

    feat: added IAlbumRepository

---
+++ Repositories/Interfaces/IAlbumRepository.cs
@@ -0,0 +1,11 @@
+﻿using PhotoGallery.Models.Entities;
+
+namespace PhotoGallery.Repositories.Interfaces;
+
+public interface IAlbumRepository
+{
+    Task<int> CreateAlbumAsync(Album albumDto);
+    Task<IEnumerable<Album>> GetAllAlbumsAsync();
+    Task<Album> GetAlbumByIdAsync(int id);
+    Task<IEnumerable<Photo>> GetPhotosByAlbumIdAsync(int albumId);
+}
